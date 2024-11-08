e4ce62f7f006f4e6cd02297e79957bba9e77361b
Psloan4 <156382704+Psloan4@users.noreply.github.com>
2024-11-08 16:16:08 -0700

    changed project structure

---
+++ StartupFiles/src/app.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Play } from './play/play';
+import { Scores } from './scores/scores';
+import { About } from './about/about';
+import { AuthState } from './login/authState';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <div className='body bg-dark text-light'>
+        <p>Testing</p>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+function NotFound() {
+  return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
+}
+
+export default App;
