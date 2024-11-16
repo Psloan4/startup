58145b695479ba4252384d0daecf7846e2c32f53
Psloan4 <156382704+Psloan4@users.noreply.github.com>
2024-11-16 10:18:13 -0700

    got login/creation working

--- StartupFiles/service/index.js
+++ StartupFiles/service/index.js
@@ -6,13 +6,15 @@ const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
 app.use(express.json());
 
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
 let users = {};
 let leaderboards = {
     "squat": [],
     "bench": [],
     "deadlift": [],
 };
-
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
@@ -31,14 +33,17 @@ apiRouter.post('/auth/create', async (req, res) => {
   });
 
   apiRouter.post('/auth/login', async (req, res) => {
+    console.log("in login")
     const user = users[req.body.email];
     if (user) {
       if (req.body.password === user.password) {
         user.token = uuid.v4();
         res.send({ token: user.token });
+        console.log(users)
         return;
       }
     }
+    console.log(users)
     res.status(401).send({ msg: 'Unauthorized' });
   });
   
@@ -48,6 +53,7 @@ apiRouter.post('/auth/create', async (req, res) => {
     if (user) {
       delete user.token;
     }
+    console.log(users)
     res.status(204).end();
   });
 
@@ -58,13 +64,14 @@ apiRouter.post('/auth/create', async (req, res) => {
   // SubmitScore
   apiRouter.post('/progress', (req, res) => {
     const user = Object.values(users).find((u) => u.token === req.body.token);
+    let newLeaderboard = {
+        score: req.body.score,
+        email: req.body.email,
+        date: req.body.date,
+    }
     if (user) {
-        user.progress.push({
-            score: req.score,
-            email: req.email,
-            date: req.date,
-        })
-        leaderboards = updateLeaderboard(req.body, scores);
+        user.progress.push(newLeaderboard)
+        leaderboards[req.lift_type] = updateLeaderboard(newLeaderboard, leaderboards[req.body.lift_type]);
     }
     res.status(204).end();
     res.send(user.progress);
@@ -74,18 +81,14 @@ apiRouter.post('/auth/create', async (req, res) => {
     let found = false;
     for (const [i, prevScore] of leaderboard.entries()) {
       if (newLeaderboard.score > leaderboard.score) {
-        scores.splice(i, 0, {
-            score: newLeaderboard.score,
-            email: newLeaderboard.email,
-            date: newLeaderboard.date,
-        });
+        scores.splice(i, 0, newLeaderboard);
         found = true;
         break;
       }
     }
   
     if (!found) {
-      scores.push(newScore);
+      scores.push(newLeaderboard);
     }
   
     if (scores.length > 5) {
