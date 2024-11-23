b243045d04d760977e97ee9a0ce51026bde654e9
Psloan4 <156382704+Psloan4@users.noreply.github.com>
2024-11-23 16:16:25 -0700

    added other functions to database.js

--- StartupFiles/service/database.js
+++ StartupFiles/service/database.js
@@ -8,4 +8,55 @@ const client = new MongoClient(url);
 const db = client.db('startup');
 
 const userCollection = db.collection('user');
-const leaderboardCollection = db.collection('leaderboards');
+const squatLeaderboardCollection = db.collection('squatLeaderboards');
+const benchLeaderboardCollection = db.collection('benchLeaderboards');
+const deadliftLeaderboardCollection = db.collection('deadliftLeaderboards');
+
+(async function testConnection() {
+    await client.connect();
+    await db.command({ ping: 1 });
+  })().catch((ex) => {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  });
+
+  async function createUser(email, password) {
+    // Hash the password before we insert it into the database
+    const passwordHash = await bcrypt.hash(password, 10);
+  
+    const user = {
+      email: email,
+      password: passwordHash,
+      token: uuid.v4(),
+    };
+    await userCollection.insertOne(user);
+  
+    return user;
+  }
+
+  async function addSquatLeaderboard(score) {
+    return squatLeaderboardCollection.insertOne(score);
+  }
+  
+  async function addBenchLeaderboard(score) {
+    return benchLeaderboardCollection.insertOne(score);
+  }
+
+  async function addDeadliftLeaderboard(score) {
+    return deadliftLeaderboardCollection.insertOne(score);
+  }
+  
+  function getSquatLeaderboard() {
+    const cursor = squatLeaderboardCollection.find(); // No query, no options
+    return cursor.toArray(); // Converts the cursor to an array of documents
+  }
+
+  function getBenchLeaderboard() {
+    const cursor = benchLeaderboardCollection.find(); // No query, no options
+    return cursor.toArray(); // Converts the cursor to an array of documents
+  }
+  
+  function getDeadliftLeaderboard() {
+    const cursor = deadliftLeaderboardCollection.find(); // No query, no options
+    return cursor.toArray(); // Converts the cursor to an array of documents
+  }
